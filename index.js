7c65cf4a413b1e0ffa9ecce5dfdd1e4a7c1b2fdc
Ayodele pelumi <adedijiayodele91@gmail.com>
2022-05-04 10:51:40 +0100

    First modification

---
+++ index.js
@@ -0,0 +1,6 @@
+const topScroll = document.getElementById('top-scroll');
+topScroll.addEventListener("click", () => {
+ document.body.scrollIntoView();
+
+//   scrollintoView is an inbuilt function for BackTOTop 
+ });
